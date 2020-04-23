ebc1e972a1929c9137ae981e315456e115651473
thechitoguyraw <64195111+thechitoguyraw@users.noreply.github.com>
2020-04-23 02:33:02 -0700

    Add files via upload

---
+++ Assets/JS/home.js
@@ -0,0 +1,53 @@
+var imgs = ['img/authentic.jpg', 'img/taco.jpg', 'img/authentic.jpg'];
+var time = 3000;
+var i = 0
+var menu = document.querySelector("#navlist")
+var collapse = document.querySelector("#hamburger")
+const nav = document.querySelector('#navbar');
+const navSet = nav.offsetTop;
+
+function stickyNav() {
+  
+
+  if (window.scrollY >= navSet) {
+    // nav offsetHeight = height of nav
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('sticky');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('sticky');
+  }
+}
+
+window.addEventListener('scroll', stickyNav);
+
+
+
+
+
+
+
+
+
+function changeSlide(){
+    document.slides.src = imgs[i];
+
+       if(i < imgs.length-1){
+           i++;
+} 
+    else{
+    i = 0;
+}
+setTimeout("changeSlide()", time);
+
+}
+
+window.onload = changeSlide;
+
+// function  toggle(){
+//   menu.classList.toggle("toggleMenu");
+//   hamburger.addEventListener('click', "toggleMenu" )
+// }
+collapse.addEventListener('click', function(){
+  menu.classList.toggle("togglemenu");
+});
